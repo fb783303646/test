f5764f7d9dda313e72ce77e0fc05245da3e566d4
fengbo <fengbo@123.com>
2017-07-09 12:11:04 +0800

    ceshi

--- blogdome/models/article.js
+++
@@ -1,5 +0,0 @@
-var mongoose = require('mongoose');
-var ArticleSchema = require('../schemas/article');
-var Article = mongoose.model('article', ArticleSchema);
- console.log('创建表');
-module.exports = Article;
